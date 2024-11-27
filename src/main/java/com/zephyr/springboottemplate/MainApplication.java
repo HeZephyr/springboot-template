1f0596a0cb4792c60b8bfdd1178e4f242064334a
zfhe <2825841950@qq.com>
2024-11-27 12:59:48 +0800

    feat: add the support of redis

--- src/main/java/com/zephyr/springboottemplate/MainApplication.java
+++ src/main/java/com/zephyr/springboottemplate/MainApplication.java
@@ -1,13 +1,18 @@
 package com.zephyr.springboottemplate;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
+/**
+ * 项目启动入口
+ */
+
+// todo 如需开启 Redis，须移除 exclude 中的内容
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class, RedisAutoConfiguration.class})
 @EnableScheduling
 @EnableAspectJAutoProxy(proxyTargetClass = true, exposeProxy = true)
 public class MainApplication {
